c4270ee7e45674105228309cf4c8ee1594b3b74d
Felpimpim007Carlin <00001115607078sp@al.educacao.sp.gov.br>
2024-09-12 14:32:36 -0300

    js dos inferno

---
+++ script.js
@@ -0,0 +1,11 @@
+const avanca= document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(botton=> {
+    botton.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttibute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
